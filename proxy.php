3a99b972b2411db5eeb7a52c1a84f0edf49f18d8
Évelyne Lachance <eslachance@gmail.com>
2015-09-17 22:49:35 -0400

    Support for gzip and chunked transfer encoding
    This update adds support for gzip encoded responses from the server. It also adds support for chunked transfer encoding, by merging those chunks together (see function at the end).
    
    PLEASE NOTE: I'm not sure why, but using the header() function currently seems to return a blank response (well, my contents was empty in my test), which means no headers from the backend are added. I'm working to resolve this issue.

--- proxy.php
+++ proxy.php
@@ -3,6 +3,7 @@
 | PROXY.PHP                                                     |
 | Created By: Évelyne Lachance                                  |
 | Contact: eslachance@gmail.com                                 |
+| Source: http://github.com/eslachance/php-transparent-proxy	|
 | Description: This proxy does a POST or GET request from any   |
 |         page on the authorized domain to the defined URL      |
 /--------------------------------------------------------------*/
@@ -11,12 +12,14 @@
 $destinationURL = 'http://www.otherdomain.com/backend.php';
 
 // The only domain from which requests are authorized.
-$RequestDomain = 'mydomain.com';
+// Must include port number (example.com:8181) if different than the default http port.
+$RequestDomain = 'example.com:8123';
 
 // That's it for configuration!
 
+// Credits to Chris Hope (http://www.electrictoolbox.com/chris-hope/) for this function.
+// http://www.electrictoolbox.com/php-get-headers-sent-from-browser/
 if(!function_exists('apache_request_headers')) {
-// Function is from: http://www.electrictoolbox.com/php-get-headers-sent-from-browser/
     function apache_request_headers() {
         $headers = array();
         foreach($_SERVER as $key => $value) {
@@ -27,8 +30,7 @@ if(!function_exists('apache_request_headers')) {
         return $headers;
     }
 }
-
-// Figure out requester's IP to shipt it to X-Forwarded-For
+// Figure out requester's IP to ship it to X-Forwarded-For
 $ip = '';
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) { 
     $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -41,29 +43,47 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     //echo "REMOTE_ADDR: ".$ip;
 }
 
+// Get the requesting domain name
 preg_match('@^(?:http://)?([^/]+)@i', $_SERVER['HTTP_REFERER'], $matches);
 $host = $matches[1];
 preg_match('/[^.]+\.[^.]+$/', $host, $matches);
 $domainName = "{$matches[0]}";
 
+// IF domain name matches the authorized domain, proceed with request.
 if($domainName == $RequestDomain) {
-
     $method = $_SERVER['REQUEST_METHOD'];
     $response = proxy_request($destinationURL, ($method == "GET" ? $_GET : $_POST), $method);
     $headerArray = explode("\r\n", $response['header']);
-
+	$is_gzip = false;
+	$is_chunked = false;
     foreach($headerArray as $headerLine) {
-     header($headerLine, FALSE);
+		// Toggle gzip decompression when appropriate.
+		if($headerLine == "Content-Encoding: gzip") {
+			global $is_gzip;
+			$is_gzip = true;
+		// Toggle chunk merging when appropriate
+		} elseif($headerLine == "Transfer-Encoding: chunked") {
+			global $is_chunked;
+			$is_chunked = true;
+		} else {
+			// todo: Find out why this doesn't work (removes all contents!)
+			// header($headerLine, FALSE);
+		}
     }
-    echo $response['content'];
- 
+	$contents = $response['content'];
+	if($is_chunked) {
+		$contents = decode_chunked($contents);
+	}
+	if($is_gzip) {
+		$contents = gzdecode($contents);
+	}
+	echo $contents;
   } else {
-
-    echo "HTTP Referer is not recognized. Cancelling all actions";
-
+    echo $domainName." is not an authorized domain.";
   }
 
-function proxy_request($url, $data, $method) {
+  
+  function proxy_request($url, $data, $method) {
 // Based on post_request from http://www.jonasjohn.de/snippets/php/post-request.htm
     global $ip;
     // Convert the data array into URL Parameters like a=b&foo=bar etc.
@@ -122,19 +142,30 @@ function proxy_request($url, $data, $method) {
  
     // close the socket connection:
     fclose($fp);
- 
+
     // split the result header from the content
     $result = explode("\r\n\r\n", $result, 2);
- 
     $header = isset($result[0]) ? $result[0] : '';
     $content = isset($result[1]) ? $result[1] : '';
- 
+
     // return as structured array:
     return array(
         'status' => 'ok',
         'header' => $header,
         'content' => $content
     );
+}
+
+// Credits to @flowfree (http://stackoverflow.com/users/1396314/flowfree) for this function.
+// http://stackoverflow.com/questions/10793017/how-to-easily-decode-http-chunked-encoded-string-when-making-raw-http-request
+function decode_chunked($str) {
+  for ($res = ''; !empty($str); $str = trim($str)) {
+    $pos = strpos($str, "\r\n");
+    $len = hexdec(substr($str, 0, $pos));
+    $res.= substr($str, $pos + 2, $len);
+    $str = substr($str, $pos + 2 + $len);
+  }
+  return $res;
 }
 
 ?>
